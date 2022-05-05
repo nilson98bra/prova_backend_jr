3a2a04c906f5863d5e869692679d66c8b7f96948
nilson98bra <nilson.98@hotmail.com>
2022-05-05 11:45:11 -0300

    base da api
    Códigos da estrutura base da API.

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require("http")
+const app = require("./res/app")
+const port = process.env.PORT || 8080
+const server = http.createServer(app)
+server.listen(port,()=>{
+    console.log("Servidor rodando na porta ",port)
+})
